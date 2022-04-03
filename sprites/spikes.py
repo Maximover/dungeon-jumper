4afda9bf5adaa4833cba1a5c7b0d904010187dd4
Maximover <72668610+Maximover@users.noreply.github.com>
2022-04-03 11:20:08 +0200

    Update spikes.py

--- sprites/spikes.py
+++ sprites/spikes.py
@@ -5,7 +5,7 @@ class Spikes(pygame.sprite.Sprite):
     def __init__(self, position, size) -> None:
         super().__init__()
         self.image = pygame.Surface((size, size/32))
-        self.image = pygame.image.load(os.path.join(ASSET_DIR, 'spikes.png'))#.subsurface(position[0], position[1]+size-1, size, size/32)
+        self.image = pygame.image.load(os.path.join(ASSET_DIR, 'spikes_tall.png'))
         self.rect = self.image.get_rect(bottomleft=position)
 
     def update(self, move_val):
